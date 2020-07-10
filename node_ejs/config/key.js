87ed9fba72d85eb5027b1aecfa5663bd68a7077d
brayan <sebastianblas679@gmail.com>
2020-07-10 17:09:16 -0500

    se acabo la parte 7

---
+++ node_ejs/config/key.js
@@ -0,0 +1,6 @@
+module.exports={
+    google:{
+        clientID: "1071108359165-1kph99r0kqhbur2d1fl5shtmr5vh55fc.apps.googleusercontent.com",
+        clientSecret: "wgscwNJ2PYkj9cohPb5w1P6C"
+    }
+};
--- node_ejs/config/passport-setup.js
+++ node_ejs/config/passport-setup.js
@@ -1,10 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const keys=require('./keys')
+
 passport.use(new GoogleStrategy({
-    clientID: "1071108359165-1kph99r0kqhbur2d1fl5shtmr5vh55fc.apps.googleusercontent.com",
-    clientSecret: "wgscwNJ2PYkj9cohPb5w1P6C",
-    callbackURL: "http://www.example.com/auth/google/callback"
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
+    //callbackURL: "http://www.example.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
