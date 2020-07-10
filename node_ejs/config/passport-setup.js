83dc2bea6c7b4cc5c20187ba4dbe17ef6192e403
brayan <sebastianblas679@gmail.com>
2020-07-10 18:05:42 -0500

    se agrego la parte 9

--- node_ejs/config/passport-setup.js
+++ node_ejs/config/passport-setup.js
@@ -7,10 +7,15 @@ passport.use(new GoogleStrategy({
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret,
     callbackURL: '/auth/google/redirect'
-  },
+  },(accessToken, refreshToken, profile, done)=>{
+    console.log('passport callback function fired');
+    console.log(profile);
+  })
+);
+/*
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
-));
+*/ 
